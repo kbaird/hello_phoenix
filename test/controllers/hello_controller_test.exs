1f061055c40fa66ade1c19d3062c402567508e26
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-15 07:57:24 -0400

    Add a fake memory-intensive test with @tag :hog; exclude that tag by default in test_helper.exs
    This tag could handle slow tests that can be run manually (or via another CI build with `--only hog`)

--- test/controllers/hello_controller_test.exs
+++ test/controllers/hello_controller_test.exs
@@ -9,4 +9,11 @@ defmodule HelloPhoenix.HelloControllerTest do
     conn = get conn(), "/hello/zoidberg"
     assert html_response(conn, 200) =~ "Hello World, from zoidberg!"
   end
+
+  @tag :hog # this would be a good way to partition resource-intensive tests that
+            # need to be run, but not necessarily by default for every suite
+  test "GET /hello/some_memory_intensive_operation" do
+    conn = get conn(), "/hello/some_memory_intensive_operation"
+    assert html_response(conn, 200) =~ "Hello World, from some_memory_intensive_operation!"
+  end
 end
