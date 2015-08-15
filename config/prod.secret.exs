c59254f61569aa64a5f3c31e27ccff834098218b
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-15 06:03:01 -0400

    Initial commit of the results of http://www.phoenixframework.org/docs/up-and-running

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  secret_key_base: "RLtXvd/zSDPbLYZHO1hLWEbDnXdGKmlzUl+CCPNrJE7HuUzIEy6QM1MlOdUOG3rR"
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "hello_phoenix_prod",
+  size: 20 # The amount of database connections in the pool
