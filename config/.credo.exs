1d2c30e1efb05ab9ca756dab04152bcea1728187
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-04-21 15:22:08 -0700

    Add cursory vs. default credo checks

--- config/.credo.exs
+++ config/.credo.exs
@@ -2,10 +2,67 @@
 #
 # If you find anything wrong or unclear in this file, please report an
 # issue on GitHub: https://github.com/rrrene/credo/issues
+
+cursory_checks = fn() -> [
+  {Credo.Check.Consistency.ExceptionNames},
+  {Credo.Check.Consistency.LineEndings},
+  {Credo.Check.Consistency.TabsOrSpaces},
+
+  # For some checks, like AliasUsage, you can only customize the priority
+  # Priority values are: `low, normal, high, higher`
+  {Credo.Check.Design.AliasUsage, priority: :low},
+  # For others you can set parameters
+  {Credo.Check.Design.DuplicatedCode, mass_threshold: 16, nodes_threshold: 2},
+
+  {Credo.Check.Readability.FunctionNames},
+  {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
+  {Credo.Check.Readability.ModuleAttributeNames},
+  {Credo.Check.Readability.ModuleDoc, false},
+  {Credo.Check.Readability.ModuleNames},
+  {Credo.Check.Readability.PredicateFunctionNames},
+  {Credo.Check.Readability.TrailingBlankLine},
+  {Credo.Check.Readability.TrailingWhiteSpace},
+  {Credo.Check.Readability.VariableNames},
+
+  {Credo.Check.Refactor.ABCSize},
+  {Credo.Check.Refactor.CaseTrivialMatches},
+  {Credo.Check.Refactor.CondStatements},
+  {Credo.Check.Refactor.FunctionArity},
+  {Credo.Check.Refactor.MatchInCondition},
+  {Credo.Check.Refactor.PipeChainStart},
+  {Credo.Check.Refactor.CyclomaticComplexity},
+  {Credo.Check.Refactor.NegatedConditionsInUnless},
+  {Credo.Check.Refactor.NegatedConditionsWithElse},
+  {Credo.Check.Refactor.Nesting},
+  {Credo.Check.Refactor.UnlessWithElse},
+
+  {Credo.Check.Warning.IExPry},
+  {Credo.Check.Warning.IoInspect},
+  {Credo.Check.Warning.NameRedeclarationByAssignment},
+  {Credo.Check.Warning.NameRedeclarationByCase},
+  {Credo.Check.Warning.NameRedeclarationByDef},
+  {Credo.Check.Warning.NameRedeclarationByFn},
+  {Credo.Check.Warning.OperationOnSameValues},
+  {Credo.Check.Warning.UnusedEnumOperation},
+  {Credo.Check.Warning.UnusedKeywordOperation},
+  {Credo.Check.Warning.UnusedListOperation},
+  {Credo.Check.Warning.UnusedStringOperation},
+  {Credo.Check.Warning.UnusedTupleOperation},
+  {Credo.Check.Warning.OperationWithConstantResult},
+] end
+
 %{
   #
   # You can have as many configs as you like in the `configs:` field.
   configs: [
+    %{
+      name: "cursory",
+      files: %{
+        included: ["lib/", "src/", "web/", "apps/"],
+        excluded: []
+      },
+      checks: cursory_checks.()
+    },
     %{
       #
       # Run any config using `mix credo -C <name>`. If no config name is given
@@ -31,16 +88,7 @@
       #
       #      {Credo.Check.Consistency.ExceptionNames, false}
       #
-      checks: [
-        {Credo.Check.Consistency.ExceptionNames},
-        {Credo.Check.Consistency.LineEndings},
-        {Credo.Check.Consistency.TabsOrSpaces},
-
-        # For some checks, like AliasUsage, you can only customize the priority
-        # Priority values are: `low, normal, high, higher`
-        {Credo.Check.Design.AliasUsage, priority: :low},
-        # For others you can set parameters
-        {Credo.Check.Design.DuplicatedCode, mass_threshold: 16, nodes_threshold: 2},
+      checks: cursory_checks.() ++ [
 
         # You can also customize the exit_status of each check.
         # If you don't want TODO comments to cause `mix credo` to fail, just
@@ -48,41 +96,6 @@
         {Credo.Check.Design.TagTODO, exit_status: 2},
         {Credo.Check.Design.TagFIXME},
 
-        {Credo.Check.Readability.FunctionNames},
-        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
-        {Credo.Check.Readability.ModuleAttributeNames},
-        {Credo.Check.Readability.ModuleDoc, false},
-        {Credo.Check.Readability.ModuleNames},
-        {Credo.Check.Readability.PredicateFunctionNames},
-        {Credo.Check.Readability.TrailingBlankLine},
-        {Credo.Check.Readability.TrailingWhiteSpace},
-        {Credo.Check.Readability.VariableNames},
-
-        {Credo.Check.Refactor.ABCSize},
-        {Credo.Check.Refactor.CaseTrivialMatches},
-        {Credo.Check.Refactor.CondStatements},
-        {Credo.Check.Refactor.FunctionArity},
-        {Credo.Check.Refactor.MatchInCondition},
-        {Credo.Check.Refactor.PipeChainStart},
-        {Credo.Check.Refactor.CyclomaticComplexity},
-        {Credo.Check.Refactor.NegatedConditionsInUnless},
-        {Credo.Check.Refactor.NegatedConditionsWithElse},
-        {Credo.Check.Refactor.Nesting},
-        {Credo.Check.Refactor.UnlessWithElse},
-
-        {Credo.Check.Warning.IExPry},
-        {Credo.Check.Warning.IoInspect},
-        {Credo.Check.Warning.NameRedeclarationByAssignment},
-        {Credo.Check.Warning.NameRedeclarationByCase},
-        {Credo.Check.Warning.NameRedeclarationByDef},
-        {Credo.Check.Warning.NameRedeclarationByFn},
-        {Credo.Check.Warning.OperationOnSameValues},
-        {Credo.Check.Warning.UnusedEnumOperation},
-        {Credo.Check.Warning.UnusedKeywordOperation},
-        {Credo.Check.Warning.UnusedListOperation},
-        {Credo.Check.Warning.UnusedStringOperation},
-        {Credo.Check.Warning.UnusedTupleOperation},
-        {Credo.Check.Warning.OperationWithConstantResult},
       ]
     }
   ]
